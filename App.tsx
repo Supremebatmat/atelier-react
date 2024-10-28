9e0d95c52cda1a3239756a15f40835815b1019e6
Arnaud Guevaer <guevaer8@gmail.com>
2024-10-28 19:22:35 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import History from "./components/History";
+import Robot from "./components/Robot";
+
+function App() {
+	return (
+		<>
+			<Header />
+			<History />
+			<Robot />
+		</>
+	);
+}
+
+export default App;
