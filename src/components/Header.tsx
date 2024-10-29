fad39ae92479d7d1efd08a2e4833006838568bbd
Mathieu_Prier <mathieu.priez@live.fr>
2024-10-29 11:09:45 +0100

    second

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,13 @@ import "./Header.css";
 function Header() {
 	return (
 		<header>
-			<img className="logo" src="src\components\logo.svg" alt="" />{" "}
-			<h1>Boston Dynamics</h1>
+			<img className="logo" src="src\components\logo.svg" alt="" />
+			<img
+				className="logoBoston"
+				src="src\components\Logo-boston-dynamics.svg"
+				alt=""
+			/>
+			<h1>Projet de groupe React</h1>
 		</header>
 	);
 }
